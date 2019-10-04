63f458886c2a4b9a98a805adaed222087a4e5763
Lars Lindberg <lars.lindberg@xlent.se>
2019-10-04 09:57:13 +0200

    Services.Implementations: 1.11.0 Added controller injection.

--- src/Services.Implementations/Adapter/Startup/NexusAdapterStartup.cs
+++ src/Services.Implementations/Adapter/Startup/NexusAdapterStartup.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nexus.Link.BusinessEvents.Sdk.RestClients;
 using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Assert;
 using Nexus.Link.Libraries.Web.AspNet.Startup;
+using Nexus.Link.Services.Contracts.Capabilities;
 using Nexus.Link.Services.Implementations.Adapter.Startup.Configuration;
 using Nexus.Link.Services.Implementations.BusinessApi.Startup;
 using Nexus.Link.Services.Contracts.Events;
+using Nexus.Link.Services.Implementations.Adapter.Capabilities.Integration;
 using Nexus.Link.Services.Implementations.Adapter.Events;
 
 #if NETCOREAPP
@@ -24,6 +30,8 @@ namespace Nexus.Link.Services.Implementations.Adapter.Startup
         /// </summary>
         protected AdapterConfiguration AdapterConfiguration { get; }
 
+        private readonly IDictionary<Type, IEnumerable<Type>> _capabilityInterfaceToControllerClasses = new Dictionary<Type, IEnumerable<Type>>();
+
         /// <inheritdoc/>
         protected NexusAdapterStartup(IConfiguration configuration) : base(configuration, false)
         {
@@ -37,28 +45,70 @@ namespace Nexus.Link.Services.Implementations.Adapter.Startup
         protected override void DependencyInjectServices(IServiceCollection services, IMvcBuilder mvcBuilder)
         {
             base.DependencyInjectServices(services, mvcBuilder);
-            DependencyInjectBusinessApiServices(services, mvcBuilder);
-            DependencyInjectAdapterServices(services, mvcBuilder);
+            DependencyInjectBusinessApiServices(services);
+            DependencyInjectAdapterServices(services);
+            AddControllersToMvc(services, mvcBuilder);
             var subscriptionHandler = new EventSubscriptionHandler();
             AddSubscriptions(subscriptionHandler);
             services.AddSingleton<IEventReceiver>(new EventReceiverLogic(subscriptionHandler));
         }
 
         /// <summary>
+        /// Register which controllers that should be used for a specific capability interface.
+        /// </summary>
+        /// <param name="capabilityInterface"></param>
+        /// <param name="controllerTypes"></param>
+        protected void RegisterCapabilityControllers(Type capabilityInterface, params Type[] controllerTypes)
+        {
+            InternalContract.Require(capabilityInterface, type => type.IsInterface, nameof(capabilityInterface));
+            InternalContract.Require(capabilityInterface.IsInterface, 
+                $"The parameter {nameof(capabilityInterface)} must be an interface.");
+            InternalContract.Require(typeof(IServicesCapability).IsAssignableFrom(capabilityInterface), 
+                $"The parameter {nameof(capabilityInterface)} must inherit from {typeof(IServicesCapability).FullName}.");
+            foreach (var controllerType in controllerTypes)
+            {
+                InternalContract.Require(controllerType, type => type.IsClass, nameof(controllerType));
+            }
+
+            _capabilityInterfaceToControllerClasses.Add(capabilityInterface, controllerTypes);
+        }
+
+        private void AddControllersToMvc(IServiceCollection services, IMvcBuilder mvcBuilder)
+        {
+            using (var serviceScope = services.BuildServiceProvider().CreateScope())
+            {
+                var serviceProvider = serviceScope.ServiceProvider;
+
+                foreach (var serviceType in _capabilityInterfaceToControllerClasses.Keys)
+                {
+                    FulcrumAssert.IsTrue(serviceType.IsInterface);
+                    var service = serviceProvider.GetService(serviceType);
+                    if (service == null) continue;
+                    var controllerTypes = _capabilityInterfaceToControllerClasses[serviceType];
+                    FulcrumAssert.IsNotNull(controllerTypes);
+                    foreach (var controllerType in controllerTypes)
+                    {
+                        FulcrumAssert.IsTrue(controllerType.IsClass);
+                        var assembly = controllerType.GetTypeInfo().Assembly;
+                        mvcBuilder.AddApplicationPart(assembly);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
         /// This is where the business API injects its own services.
         /// </summary>
         /// <param name="services">From the parameter to Startup.ConfigureServices.</param>
-        /// <param name="mvcBuilder"></param>
         /// <remarks>Always override this to inject your services.</remarks>
-        protected abstract void DependencyInjectBusinessApiServices(IServiceCollection services, IMvcBuilder mvcBuilder);
+        protected abstract void DependencyInjectBusinessApiServices(IServiceCollection services);
 
         /// <summary>
         /// This is where the adapter injects its own services.
         /// </summary>
         /// <param name="services">From the parameter to Startup.ConfigureServices.</param>
-        /// <param name="mvcBuilder"></param>
         /// <remarks>Always override this to inject your services.</remarks>
-        protected abstract void DependencyInjectAdapterServices(IServiceCollection services, IMvcBuilder mvcBuilder);
+        protected abstract void DependencyInjectAdapterServices(IServiceCollection services);
 
         /// <summary>
         /// This is where the adapter can add events that it wants to subscribe to.
