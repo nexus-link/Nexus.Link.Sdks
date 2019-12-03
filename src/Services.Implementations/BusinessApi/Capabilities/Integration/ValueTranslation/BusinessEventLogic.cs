5ba002189fcb58a7d4bfa5ae228c40c691673e5f
Lars Lindberg <lars.lindberg@xlent.se>
2019-12-03 15:57:26 +0100

    Services.Implementations: 3.2.0 Added ValueTranslationCapability.

---
+++ src/Services.Implementations/BusinessApi/Capabilities/Integration/ValueTranslation/BusinessEventLogic.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Assert;
+using Nexus.Link.Libraries.Core.Error.Logic;
+using Nexus.Link.Libraries.Core.Json;
+using Nexus.Link.Libraries.Core.Misc;
+using Nexus.Link.Services.Contracts.Capabilities.Integration.BusinessEvents;
+using Nexus.Link.Services.Contracts.Events;
+
+namespace Nexus.Link.Services.Implementations.BusinessApi.Capabilities.Integration.ValueTranslation
+{
+    /// <inheritdoc />
+    public class BusinessEventLogic : IBusinessEventService
+    {
+        private static Link.BusinessEvents.Sdk.BusinessEvents _businessEventsService;
+
+        /// <summary>
+        /// Must be set if the business API should publish events itself as publisher.
+        /// </summary>
+        public bool ForInternalUseInBusinessApi { get; set; }
+
+        /// <inheritdoc />
+        public BusinessEventLogic(string serviceBaseUrl, ServiceClientCredentials serviceClient)
+        {
+            _businessEventsService =
+                new Link.BusinessEvents.Sdk.BusinessEvents(serviceBaseUrl, FulcrumApplication.Setup.Tenant, serviceClient);
+        }
+
+        /// <inheritdoc />
+        public Task PublishAsync(JToken eventAsJson, CancellationToken token = default(CancellationToken))
+        {
+            InternalContract.RequireNotNull(eventAsJson, nameof(eventAsJson));
+            var @event = JsonHelper.SafeDeserializeObject<PublishableEvent>(eventAsJson.ToString(Formatting.None));
+            InternalContract.RequireNotNull(@event, nameof(@event));
+            InternalContract.RequireNotNull(@event?.Metadata, nameof(@event.Metadata));
+            InternalContract.RequireValidated(@event?.Metadata, nameof(@event.Metadata));
+            // TODO: Make the internal name for BusinessApi a configurable name
+            var publisherName = ForInternalUseInBusinessApi
+                ? "business-api"
+                : FulcrumApplication.Context?.ClientPrincipal?.Identity?.Name;
+            FulcrumAssert.IsNotNullOrWhiteSpace(publisherName, CodeLocation.AsString());
+            var metadata = @event?.Metadata;
+            if (metadata == null) throw new FulcrumAssertionFailedException("Metadata was unexpectedly null.");
+            return _businessEventsService.PublishAsync(
+                metadata.EntityName,
+                metadata.EventName,
+                metadata.MajorVersion,
+                metadata.MinorVersion,
+                publisherName,
+                eventAsJson);
+        }
+    }
+}
---
+++ src/Services.Implementations/BusinessApi/Capabilities/Integration/ValueTranslation/ValueTranslationCapability.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Rest;
+using Nexus.Link.KeyTranslator.Sdk;
+using Nexus.Link.KeyTranslator.Sdk.RestClients.Facade.Clients;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Assert;
+using Nexus.Link.Libraries.Core.Translation;
+using Nexus.Link.Services.Contracts.Capabilities.Integration.ValueTranslation;
+
+namespace Nexus.Link.Services.Implementations.BusinessApi.Capabilities.Integration.ValueTranslation
+{
+    /// <inheritdoc />
+    public class ValueTranslationCapability : IValueTranslationCapability
+    {
+        /// <inheritdoc />
+        public ValueTranslationCapability(string serviceBaseUrl, ServiceClientCredentials serviceClientCredentials)
+        {
+            InternalContract.RequireNotNullOrWhiteSpace(serviceBaseUrl, nameof(serviceBaseUrl));
+            InternalContract.RequireNotNull(serviceClientCredentials, nameof(serviceClientCredentials));
+            var translatorClient = new TranslateClient(serviceBaseUrl, FulcrumApplication.Setup.Tenant, serviceClientCredentials);
+            TranslatorService = new TranslatorService(translatorClient);
+        }
+
+        /// <inheritdoc />
+        public ITranslatorService TranslatorService { get; }
+    }
+}
