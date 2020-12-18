842228abfe412929a8dcbd5a88c5d0fa98b539fa
Lars Lindberg <lars.lindberg@xlent.se>
2020-12-18 08:33:03 +0100

    2.1.2 Azure storage queue doesn't accept InitialVisibilityDelay >= 7 days

--- test/AsyncCaller.UnitTests/AyncCallTests.cs
+++ test/AsyncCaller.UnitTests/AyncCallTests.cs
@@ -5,6 +5,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Nexus.Link.AsyncCaller.Sdk;
 using Nexus.Link.AsyncCaller.Sdk.Data.Models;
+using Nexus.Link.AsyncCaller.Sdk.Data.Queues;
+using Nexus.Link.AsyncCaller.Sdk.Storage.Queue;
 using Nexus.Link.Libraries.Core.Application;
 
 namespace AsyncCaller.Sdk.UnitTests
@@ -53,6 +55,35 @@ namespace AsyncCaller.Sdk.UnitTests
             await asyncCall.ExecuteAsync();
             Assert.IsNotNull(foundRawRequest);
             Assert.AreEqual(2, foundRawRequest.Priority);
+        }
+
+        [TestMethod]
+        public async Task MaxDelayTimeAccordingToAzureStorageQueue()
+        {
+            // Arrange
+            TimeSpan? actualTimeSpan = null;
+            var queueMock = new Mock<IQueue>();
+            queueMock
+                .Setup(ac => ac.AddMessageAsync(It.IsAny<string>(), It.Is<TimeSpan?>(span => span != null)))
+                .Callback((string s, TimeSpan? ts) => actualTimeSpan = ts)
+                .Returns(Task.CompletedTask);
+            queueMock
+                .Setup(ac => ac.MaybeCreateAndConnect(It.IsAny<string>()));
+
+            var requestQueue = new RequestQueue(queueMock.Object, nameof(AsyncCall));
+            var envelope = new RawRequestEnvelope
+            {
+                Attempts = 1000, // Big number to ensure that we get a long delay
+                RawRequest = new RawRequest{Id = null}
+            };
+
+            // Act
+            await requestQueue.RequeueAsync(envelope, null);
+
+            // Assert
+            Assert.IsNotNull(actualTimeSpan);
+            var expectedTimeSpan = TimeSpan.FromDays(7) - TimeSpan.FromSeconds(1);
+            Assert.AreEqual(expectedTimeSpan, actualTimeSpan);
         }
     }
 }
