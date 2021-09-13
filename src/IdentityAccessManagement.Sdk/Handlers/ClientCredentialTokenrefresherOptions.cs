9f659ed3586f534eae27e30b16c5421468a026e1
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2021-09-13 11:45:42 +0200

    IAM 0.2.0: Added TokenRefresher

---
+++ src/IdentityAccessManagement.Sdk/Handlers/ClientCredentialTokenrefresherOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityAccessManagement.Sdk.Handlers
+{
+    public class ClientCredentialTokenrefresherOptions
+    {
+        public string Authority { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string Scope { get; set; }
+    }
+}
