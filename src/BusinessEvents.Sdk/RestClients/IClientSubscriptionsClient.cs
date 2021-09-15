e64495a0eb45e4a68b983ad35f6438ecc080df51
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2021-09-15 10:27:50 +0200

    BE: 3.1.3 Refactored ClientSubscriptionsClient

--- src/BusinessEvents.Sdk/RestClients/IClientSubscriptionsClient.cs
+++ src/BusinessEvents.Sdk/RestClients/IClientSubscriptionsClient.cs
@@ -5,7 +5,7 @@ using Nexus.Link.BusinessEvents.Sdk.RestClients.Models;
 
 namespace Nexus.Link.BusinessEvents.Sdk.RestClients
 {
-    internal interface ISubscriptionsClient
+    internal interface IClientSubscriptionsClient
     {
         Task RegisterSubscriptions(string clientName, List<ClientSubscription> clientSubscriptions, CancellationToken cancellationToken = default);
     }
