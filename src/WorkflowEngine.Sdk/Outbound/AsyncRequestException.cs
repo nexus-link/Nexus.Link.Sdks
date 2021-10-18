e2f52be9c2bfa815a2ff08c08847b85d4f121376
Lars Lindberg <lars.lindberg@xlent.se>
2021-10-18 12:58:42 +0200

    New exceptions.

---
+++ src/WorkflowEngine.Sdk/Outbound/AsyncRequestException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nexus.Link.WorkflowEngine.Sdk.Outbound
+{
+    internal class AsyncRequestException : Exception
+    {
+        public string RequestId { get; }
+
+        public AsyncRequestException(string requestId)
+        {
+            RequestId = requestId;
+        }
+    }
+}
