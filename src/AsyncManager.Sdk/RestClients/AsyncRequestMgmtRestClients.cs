1fcc4f5b65210d9fbdf650e52df58573cafd752d
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2021-10-20 15:12:28 +0200

    AM sdk 1.2.0-rc01 IExecutionService, IExecutionResponseService; don't use Tenant in rest clients

--- src/AsyncManager.Sdk/RestClients/AsyncRequestMgmtRestClients.cs
+++ src/AsyncManager.Sdk/RestClients/AsyncRequestMgmtRestClients.cs
@@ -14,11 +14,20 @@ namespace Nexus.Link.AsyncManager.Sdk.RestClients
         {
             Request = new RequestRestClient(httpSender);
             RequestResponse = new RequestResponseRestClient(httpSender);
+            Execution = new ExecutionRestClient(httpSender);
+            ExecutionResponse = new ExecutionResponseRestClient(httpSender);
         }
+        
         /// <inheritdoc />
         public IRequestService Request { get; }
 
         /// <inheritdoc />
         public IRequestResponseService RequestResponse { get; }
+
+        /// <inheritdoc />
+        public IExecutionService Execution { get; }
+
+        /// <inheritdoc />
+        public IExecutionResponseService ExecutionResponse { get; }
     }
 }
