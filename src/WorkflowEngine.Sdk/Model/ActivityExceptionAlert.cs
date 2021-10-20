fe6da3d8edcc0d06d147d4edf1f7d2abff63260a
Lars Lindberg <lars.lindberg@xlent.se>
2021-10-20 15:13:21 +0200

    Introduced IActivityExceptionAlertHandler

---
+++ src/WorkflowEngine.Sdk/Model/ActivityExceptionAlert.cs
@@ -0,0 +1,16 @@
+﻿using Nexus.Link.Capabilities.WorkflowMgmt.Abstract.Entities;
+
+namespace Nexus.Link.WorkflowEngine.Sdk.Model
+{
+    public class ActivityExceptionAlert
+    {
+        public string WorkflowInstanceId { get; set; }
+        public string ActivityInstanceId { get; set; }
+
+        public ActivityExceptionCategoryEnum ExceptionCategory { get; set; }
+
+        public string ExceptionTechnicalMessage { get; set; }
+
+        public string ExceptionFriendlyMessage { get; set; }
+    }
+}
