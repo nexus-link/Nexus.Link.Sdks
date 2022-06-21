0c002915d3b10a44e0a634ddcf5c957ff8204bf9
Lars Lindberg <lars.lindberg@xlent.se>
2022-06-21 16:52:27 +0200

    Added test project Misc.AspNet.Sdk.UnitTests

---
+++ test/Misc.AspNet.Sdk.UnitTests/Inbound/NexusLinkMiddleware/Support.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using Nexus.Link.Libraries.Web.Pipe;
+
+namespace Misc.AspNet.Sdk.UnitTests.Inbound.NexusLinkMiddleware
+{
+    internal static class Support
+    {
+        public static void SetRequest(this DefaultHttpContext context, string url = "http://example.com/Person/123?test=1", string method = "GET", JObject jObject = null)
+        {
+            var match = Regex.Match(url, "^(https?)://([^/]+)(/[^?]*)?(\\?.*)?$");
+            if (!match.Success)
+            {
+                throw new ApplicationException($"The url was formatted unexpectedly: {url}");
+            }
+            var memoryStream = jObject == null ? new MemoryStream() : new MemoryStream(Encoding.UTF8.GetBytes(jObject.ToString()));
+
+            context.Request.Scheme = match.Groups[1].Value;
+            context.Request.Host = new HostString(match.Groups[2].Value);
+            context.Request.PathBase = new PathString("/");
+            context.Request.Path = new PathString(match.Groups[3].Value);
+            context.Request.Method = method;
+            context.Request.Body = memoryStream;
+            context.Request.ContentLength = memoryStream.Length;
+            context.Request.QueryString = new QueryString(match.Groups[4].Value);
+        }
+        public static void SetRequestWithReentryAuthentication(this DefaultHttpContext context, string reentryAuthentication)
+        {
+            var memoryStream = new MemoryStream();
+            context.Request.Scheme = "https";
+            context.Request.Host = new HostString("host.example.com");
+            context.Request.PathBase = new PathString("/");
+            context.Request.Path = new PathString("/Person/123");
+            context.Request.Method = "Get";
+            context.Request.Body = memoryStream;
+            context.Request.ContentLength = memoryStream.Length;
+            context.Request.QueryString = new QueryString("?id=23");
+            if (reentryAuthentication != null)
+            {
+                context.Request.Headers.Add(Constants.ReentryAuthenticationHeaderName, reentryAuthentication);
+            }
+        }
+    }
+}
---
+++ test/Misc.AspNet.Sdk.UnitTests/UrlLinkHelperTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Web.AspNet.Controllers;
+
+namespace Misc.AspNet.Sdk.UnitTests
+{
+    [TestClass]
+    public class UrlLinkHelperTests
+    {
+        public const string RouteName = "GetOneBanana";
+        public const string Route = "api/bananas/{id}";
+
+        [TestInitialize]
+        public void TestCaseInitialize()
+        {
+            FulcrumApplicationHelper.UnitTestSetup(typeof(UrlLinkHelperTests).FullName);
+        }
+
+        #region Helpers
+
+        private static UrlHelper CreateUrlHelper(string baseUrl)
+        {
+            var uri = new Uri(baseUrl);
+            var httpContext = new DefaultHttpContext()
+            {
+                Request =
+                {
+                    Host = new HostString(uri.Host, uri.Port),
+                    Path = new PathString(uri.PathAndQuery),
+                    Scheme = uri.Scheme
+                }
+            };
+            var routeData = new RouteData { Routers = { new MockRouter() } };
+            var actionContext = new ActionContext(httpContext, routeData, new ActionDescriptor());
+            return new UrlHelper(actionContext);
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void ModifyToHttps()
+        {
+            var urlHelper = CreateUrlHelper("http://example.com");
+            var uri = new Uri(urlHelper.LinkWithEnforcedHttps(RouteName, new { id = "33" }));
+
+            Assert.IsTrue(uri.Scheme == Uri.UriSchemeHttps);
+            Assert.IsFalse(uri.IsLoopback);
+            Assert.IsTrue(uri.PathAndQuery.EndsWith("/33"));
+        }
+
+        [TestMethod]
+        public void RemovePort()
+        {
+            var urlHelper = CreateUrlHelper("http://example.com:80");
+            var uri = new Uri(urlHelper.LinkWithEnforcedHttps(RouteName, new { id = "33" }));
+
+            Assert.IsTrue(uri.Scheme == Uri.UriSchemeHttps);
+            Assert.IsFalse(uri.IsLoopback);
+            Assert.IsTrue(uri.PathAndQuery.EndsWith("/33"));
+            Assert.AreEqual(443, uri.Port);
+            Assert.IsFalse(uri.PathAndQuery.Contains(":80"));
+        }
+
+        [TestMethod]
+        public void NoModificationOnLocalHost1()
+        {
+            var urlHelper = CreateUrlHelper("http://localhost");
+            var uri = new Uri(urlHelper.LinkWithEnforcedHttps(RouteName, new { id = "99" }));
+
+            Assert.IsFalse(uri.Scheme == Uri.UriSchemeHttps);
+            Assert.IsTrue(uri.IsLoopback);
+            Assert.IsTrue(uri.PathAndQuery.EndsWith("/99"));
+        }
+
+        [TestMethod]
+        public void NoModificationOnLocalHost2()
+        {
+            var urlHelper = CreateUrlHelper("http://127.0.0.1");
+            var uri = new Uri(urlHelper.LinkWithEnforcedHttps(RouteName, new { id = "99" }));
+
+            Assert.IsFalse(uri.Scheme == Uri.UriSchemeHttps);
+            Assert.IsTrue(uri.IsLoopback);
+            Assert.IsTrue(uri.PathAndQuery.EndsWith("/99"));
+        }
+
+        [TestMethod]
+        public void NoModificationOnNexusLocal()
+        {
+            var urlHelper = CreateUrlHelper("http://nexus.local");
+            var uri = new Uri(urlHelper.LinkWithEnforcedHttps(RouteName, new { id = "99" }));
+
+            Assert.IsFalse(uri.Scheme == Uri.UriSchemeHttps);
+            Assert.IsTrue(uri.PathAndQuery.EndsWith("/99"));
+        }
+
+        [TestMethod]
+        public void NoModificationWhenHttps()
+        {
+            var urlHelper = CreateUrlHelper("https://example.com");
+            var uri = new Uri(urlHelper.LinkWithEnforcedHttps(RouteName, new { id = "66" }));
+
+            Assert.IsTrue(uri.Scheme == Uri.UriSchemeHttps);
+            Assert.IsFalse(uri.IsLoopback);
+            Assert.IsTrue(uri.PathAndQuery.EndsWith("/66"));
+        }
+
+        [TestMethod]
+        public void NoModificationWhenHttpsOnLocalhost1()
+        {
+            var urlHelper = CreateUrlHelper("https://localhost");
+            var uri = new Uri(urlHelper.LinkWithEnforcedHttps(RouteName, new { id = "11" }));
+
+            Assert.IsTrue(uri.Scheme == Uri.UriSchemeHttps);
+            Assert.IsTrue(uri.IsLoopback);
+            Assert.IsTrue(uri.PathAndQuery.EndsWith("/11"));
+        }
+
+        [TestMethod]
+        public void NoModificationWhenHttpsOnLocalhost2()
+        {
+            var urlHelper = CreateUrlHelper("https://127.0.0.1");
+            var uri = new Uri(urlHelper.LinkWithEnforcedHttps(RouteName, new { id = "11" }));
+
+            Assert.IsTrue(uri.Scheme == Uri.UriSchemeHttps);
+            Assert.IsTrue(uri.IsLoopback);
+            Assert.IsTrue(uri.PathAndQuery.EndsWith("/11"));
+        }
+    }
+
+    #region Mocks
+
+    internal class MockRouter : IRouter
+    {
+        public Task RouteAsync(RouteContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            Assert.AreEqual(UrlLinkHelperTests.RouteName, context.RouteName);
+            // There is probably a better way to achieve this without implementing an IRouter...
+            return new VirtualPathData(this, UrlLinkHelperTests.Route.Replace("{id}", (string)context.Values.First().Value));
+        }
+    }
+
+    #endregion
+}
