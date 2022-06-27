b4900a85f4054d138c71d1fdda0b8b0609146edf
Lars Lindberg <lars.lindberg@xlent.se>
2022-06-27 08:47:18 +0200

    Added SetNexusLinkDefaults to NexusLinkHandlerOptions and NexusLinkMiddlewareOptions

--- src/Misc.AspNet.Sdk/Inbound/NexusLinkMiddleWareOptions.cs
+++ src/Misc.AspNet.Sdk/Inbound/NexusLinkMiddleWareOptions.cs
@@ -1,4 +1,5 @@
 ﻿using Nexus.Link.Libraries.Core.Assert;
+using Nexus.Link.Libraries.Core.Logging;
 using Nexus.Link.Misc.AspNet.Sdk.Inbound.Options;
 
 namespace Nexus.Link.Misc.AspNet.Sdk.Inbound
@@ -12,6 +13,36 @@ namespace Nexus.Link.Misc.AspNet.Sdk.Inbound
         /// The features that can be controlled.
         /// </summary>
         public MiddlewareFeatures Features = new MiddlewareFeatures();
+
+        /// <summary>
+        /// Set the default values as recommended by Nexus Link
+        /// </summary>
+        public NexusLinkMiddlewareOptions SetNexusLinkDefaults()
+        {
+            // Save information from important headers
+            Features.SaveReentryAuthentication.Enabled = true;
+            Features.SaveExecutionId.Enabled = true;
+            Features.SaveNexusTestContext.Enabled = true;
+            Features.SaveClientTenant.Enabled = true;
+            Features.SaveCorrelationId.Enabled = true;
+            Features.SaveTenantConfiguration.Enabled = true;
+
+            // This is only used by workflow engine users
+            Features.RedirectAsynchronousRequests.Enabled = false;
+
+            // Log request and response
+            Features.LogRequestAndResponse.Enabled = true;
+
+            // Use batch logs to filter out chatty logs when successful
+            Features.BatchLog.Enabled = true;
+            Features.BatchLog.FlushAsLateAsPossible = true;
+            Features.BatchLog.Threshold = LogSeverityLevel.Warning;
+
+            // Convert exceptions to the proper HTTP status code and content
+            Features.ConvertExceptionToHttpResponse.Enabled = true;
+            return this;
+        }
+
         /// <inheritdoc />
         public virtual void Validate(string errorLocation, string propertyPath = "")
         {
--- src/Misc.Web.Sdk/Outbound/NexusLinkHandlerOptions.cs
+++ src/Misc.Web.Sdk/Outbound/NexusLinkHandlerOptions.cs
@@ -13,6 +13,34 @@ namespace Nexus.Link.Misc.Web.Sdk.Outbound
         /// </summary>
         public HandlerFeatures Features = new HandlerFeatures();
 
+        /// <summary>
+        /// Set the default values as recommended by Nexus Link
+        /// </summary>
+        public NexusLinkHandlerOptions SetNexusLinkDefaults()
+        {
+            // Forward headers
+            Features.ForwardNexusUserAuthorization.Enabled = true;
+            Features.ForwardNexusTranslatedUserId.Enabled = true;
+            Features.ForwardNexusTestContext.Enabled = true;
+            Features.ForwardCorrelationId.Enabled = true;
+
+            // Convert HTTP status codes to FulcrumException
+            Features.ThrowFulcrumExceptionOnFail.Enabled = true;
+
+            // Log request and response
+            Features.LogRequestAndResponse.Enabled = true;
+
+            // Handle execution information
+            Features.HandleExecutionInformation.Enabled = true;
+            Features.HandleExecutionInformation.SaveBeforeExecutionAsyncDelegate = null;
+            Features.HandleExecutionInformation.SaveAfterExecutionAsyncDelegate = null;
+
+            // This is up to the implementor
+            Features.CustomSendDelegate.Enabled = false;
+
+            return this;
+        }
+
         /// <inheritdoc />
         public virtual void Validate(string errorLocation, string propertyPath = "")
         {
