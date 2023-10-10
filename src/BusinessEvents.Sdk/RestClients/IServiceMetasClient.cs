74329db720e0ee45a7c27d584a32d20ee4ac4821
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2023-10-10 13:23:19 +0200

    3.3.0 Expose /ServiceMetas/Health

--- src/BusinessEvents.Sdk/RestClients/IServiceMetasClient.cs
+++ src/BusinessEvents.Sdk/RestClients/IServiceMetasClient.cs
@@ -1,8 +1,15 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Nexus.Link.Libraries.Core.Health.Model;
+using Nexus.Link.Libraries.Core.MultiTenant.Model;
 
 namespace Nexus.Link.BusinessEvents.Sdk.RestClients
 {
     internal interface IServiceMetasClient : IResourceHealth
     {
+        /// <summary>
+        /// Get Business Events health for a tenant 
+        /// </summary>
+        Task<Health> TenantHealthAsync(Tenant tenant, CancellationToken cancellationToken = default);
     }
 }
