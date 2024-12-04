00c8cf9adb01b03a408cd4fdf345f980cab78c5d
Lars Lindberg <lars.lindberg@xlent.se>
2024-12-04 11:27:02 +0100

    WorkflowEngine.Sdk: 6.34.0 Added support for Serilog logger

---
+++ src/WorkflowEngine.Sdk/Support/SerilogLogger.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace Nexus.Link.WorkflowEngine.Sdk.Support
+{
+    /// <summary>
+    /// A convenience class for logging.
+    /// </summary>
+    public class SerilogLogger(ILogger logger) : ISyncLogger, IFallbackLogger, IAsyncLogger
+    {
+        /// <inheritdoc />
+        public void SafeLog(LogSeverityLevel logSeverityLevel, string message)
+        {
+            PrivateSafeLog(logSeverityLevel, message, null);
+        }
+
+        private void PrivateSafeLog(LogSeverityLevel logSeverityLevel, string message, Exception exception = null)
+        {
+            try
+            {
+                LogEventLevel logLevel;
+                switch (logSeverityLevel)
+                {
+                    case LogSeverityLevel.Verbose:
+                        logLevel = LogEventLevel.Verbose;
+                        break;
+                    case LogSeverityLevel.Information:
+                        logLevel = LogEventLevel.Information;
+                        break;
+                    case LogSeverityLevel.Warning:
+                        logLevel = LogEventLevel.Warning;
+                        break;
+                    case LogSeverityLevel.Error:
+                        logLevel = LogEventLevel.Error;
+                        break;
+                    case LogSeverityLevel.Critical:
+                        logLevel = LogEventLevel.Fatal;
+                        break;
+                    // ReSharper disable once RedundantCaseLabel
+                    case LogSeverityLevel.None:
+                        return;
+                    default:
+                        logger.Write(LogEventLevel.Fatal, $"Unexpected {nameof(logSeverityLevel)} ({logSeverityLevel}) for message:\r{message}.");
+                        return;
+                }
+                logger.Write(logLevel, exception, message);
+            }
+            catch (Exception)
+            {
+                // This method should never fail.
+            }
+        }
+
+        /// <inheritdoc />
+        public void LogSync(LogRecord logRecord)
+        {
+            PrivateSafeLog(logRecord.SeverityLevel, ToLogString(logRecord), logRecord.Exception);
+        }
+
+        /// <inheritdoc />
+        public Task LogAsync(LogRecord logRecord, CancellationToken cancellationToken = default)
+        {
+            LogSync(logRecord);
+            return Task.CompletedTask;
+        }
+
+        private string ToLogString(LogRecord logRecord)
+        {
+            var correlationId = FulcrumApplication.Context.CorrelationId;
+            var stringBuilder = new StringBuilder(logRecord.TimeStamp.ToLogString());
+            if (!string.IsNullOrWhiteSpace(correlationId)) stringBuilder.Append($" {correlationId}");
+            stringBuilder.Append(" ");
+            stringBuilder.Append(logRecord.SeverityLevel.ToString());
+            var context = ContextToLogString();
+            if (!string.IsNullOrWhiteSpace(context))
+            {
+                stringBuilder.Append(" ");
+                stringBuilder.AppendLine(context);
+            }
+            else
+            {
+                stringBuilder.AppendLine("");
+            }
+            stringBuilder.Append(logRecord.Message);
+            return stringBuilder.ToString();
+        }
+
+        private string ContextToLogString()
+        {
+            var stringBuilder = new StringBuilder($"{FulcrumApplication.Setup.Tenant} {FulcrumApplication.Setup.Name} ({FulcrumApplication.Setup.RunTimeLevel}) context:{ FulcrumApplication.Context.ContextId}");
+
+            var clientTenant = FulcrumApplication.Context.ClientTenant;
+            if (clientTenant != null)
+            {
+                stringBuilder.Append(" tenant: ");
+                stringBuilder.Append(clientTenant.ToLogString());
+            }
+
+            var clientName = FulcrumApplication.Context.CallingClientName;
+            if (!string.IsNullOrWhiteSpace(clientName))
+            {
+                stringBuilder.Append(" client: ");
+                stringBuilder.Append(clientName);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
+
---
+++ test/WorkflowEngine.Sdk.UnitTests/Internal/Support/LogTests.cs
@@ -0,0 +1,89 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Logging;
+using Nexus.Link.WorkflowEngine.Sdk.Support;
+using Serilog;
+using WorkflowEngine.Sdk.UnitTests.Internal.Activities;
+using WorkflowEngine.Sdk.UnitTests.TestSupport;
+using Xunit;
+using Xunit.Abstractions;
+using Log = Nexus.Link.Libraries.Core.Logging.Log;
+
+// ReSharper disable MethodSupportsCancellation
+
+#pragma warning disable CS0618
+
+namespace WorkflowEngine.Sdk.UnitTests.Internal.Support;
+
+public class LogTests
+{
+    public LogTests(ITestOutputHelper testOutputHelper)
+    {
+        FulcrumApplicationHelper.UnitTestSetup(nameof(ActivitySwitchTests));
+    }
+
+    [Fact]
+    public async Task Log_1_GivenVanilla_Given_Logged()
+    {
+        // Arrange
+        SetupLogger(nameof(Log_1_GivenVanilla_Given_Logged));
+
+        // Act
+        Log.LogInformation("Vanilla log");
+        await Task.Delay(TimeSpan.FromMilliseconds(300));
+    }
+
+    [Fact]
+    public Task Log_2_Given_LogAfterOperationCancelled_Given_StillLogged()
+    {
+        // Arrange
+        SetupLogger(nameof(Log_2_Given_LogAfterOperationCancelled_Given_StillLogged));
+        var cancellationTokenSource = new CancellationTokenSource();
+        Log.LogInformation("Before cancellation");
+        cancellationTokenSource.Cancel();
+
+        // Act
+        var task = LogInBackgroundAsync(cancellationTokenSource.Token);
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task Log_3_WaitForSeconds()
+    {
+        await Task.Delay(TimeSpan.FromMilliseconds(300));
+    }
+
+    private async Task LogInBackgroundAsync(CancellationToken cancellationToken)
+    {
+        Log.LogInformation("Background 1");
+        await Task.Delay(TimeSpan.FromMilliseconds(100));
+        Log.LogInformation("Background 2");
+        await Task.Delay(TimeSpan.FromMilliseconds(100));
+        Log.LogInformation("Background 3");
+        try
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            Log.LogError(ex.Message, ex);
+            throw;
+        }
+    }
+
+    private void SetupLogger(string name)
+    {
+        var tempFilePath = Path.Combine(Path.GetTempPath(), $"{name}.txt");
+        if (File.Exists(tempFilePath))
+        {
+            File.Delete(tempFilePath); // Ensure the file is clean before the test
+        }
+        var logger = new LoggerConfiguration()
+            .WriteTo.File(tempFilePath)
+            .CreateLogger();
+        FulcrumApplication.Setup.SynchronousFastLogger = new SerilogLogger(logger);
+    }
+}
